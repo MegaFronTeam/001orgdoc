357bfce3d6444140146593b04dac94a8578f971f
Pavel <wol1414@gmail.com>
2019-12-30 16:52:05 +0300

    main done

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -183,33 +183,37 @@ function eventHandler() {
 		autoplaySpeed: 6000,
 		lazyLoad: 'progressive',
 	};
-	$('.s-docs__slider--js').slick({
-		...defaultSlide,
-
-		slidesToShow: 2,
-		appendArrows: '.s-docs .arrow-wrap',
-		responsive: [{
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 5,
-			}
-		}, {
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 4,
-			}
-
-		}, {
-			breakpoint: 576,
-			settings: {
-				slidesToShow: 3,
-				arrows: true,
-			}
+	$('.section').each(function () {
 
 
-		}],
+		$(this).find('.slider--js').slick({
+			...defaultSlide,
 
-	});
+			slidesToShow: 2,
+			appendArrows: $(this).find('.arrow-wrap'),
+			responsive: [{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 5,
+				}
+			}, {
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 4,
+				}
+
+			}, {
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 3,
+					arrows: true,
+				}
+
+
+			}],
+
+		});
+	})
 	// $('.s-gal__slider\
 	// ,.slider-for2 ')
 	// 	.on('lazyLoaded', function (event, slick, image, imageSource) {
