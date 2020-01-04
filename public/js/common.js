7d6880e11f6432be539c08f2aaf4f4341fe8bbd0
Aleksandr3012 <flaming541@gmail.com>
2020-01-04 13:56:28 +0200

    07-news done

--- public/js/common.js
+++ public/js/common.js
@@ -120,7 +120,7 @@ function eventHandler() {
 	JSCCommon.inputMask(); // JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
 
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/2.png);"></div>'); // /добавляет подложку для pixel perfect
+	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/7.png);"></div>'); // /добавляет подложку для pixel perfect
 	// /закрыть/открыть мобильное меню
 
 	function heightses() {
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 1. <https://pashawol.github.io/001orgdoc/public/index.html>
 1. <https://pashawol.github.io/001orgdoc/public/02-servises.html>
 1. <https://pashawol.github.io/001orgdoc/public/05-internal-work.html>
+1. <https://pashawol.github.io/001orgdoc/public/07-news.html>
