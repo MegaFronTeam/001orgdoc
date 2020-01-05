c23ab2748ee30dd17a51211d6d051324dd236260
Aleksandr3012 <flaming541@gmail.com>
2020-01-05 13:24:20 +0200

    02 03 05 07 pixel perfect

--- public/js/common.js
+++ public/js/common.js
@@ -120,7 +120,7 @@ function eventHandler() {
 	JSCCommon.inputMask(); // JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
 
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/5.png);"></div>'); // /добавляет подложку для pixel perfect
+	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/3.png);"></div>'); // /добавляет подложку для pixel perfect
 	// /закрыть/открыть мобильное меню
 
 	function heightses() {
